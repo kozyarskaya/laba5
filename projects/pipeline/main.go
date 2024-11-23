a08734b8ea549b0409e484a7d984f8b3e334e55f
kozyarskaya <92299254+kozyarskaya@users.noreply.github.com>
2024-11-23 09:15:14 +0300

    Update main.go

--- projects/pipeline/main.go
+++ projects/pipeline/main.go
@@ -1,7 +1,29 @@
 package main
 
-// реализовать removeDuplicates(in, out chan string)
+func removeDuplicates(inputStream chan string, outputStream chan string) {
+	f := " "
+	for v := range inputStream {
+		if v != f || f == " " {
+			outputStream <- v
+		}
+		f = v
+	}
+	close(outputStream)
+}
 
 func main() {
-	// здесь должен быть код для проверки правильности работы функции removeDuplicates(in, out chan string)
+	channel1 := make(chan string, 3)
+	channel2 := make(chan string, 3)
+	str1 := "qw"
+	str2 := "qw"
+	str3 := "jnkj"
+	channel1 <- str1
+	channel1 <- str2
+	channel1 <- str3
+	close(channel1)
+	removeDuplicates(channel1, channel2)
+	for v := range channel2 {
+		fmt.Print(v, "; ")
+	}
+
 }
